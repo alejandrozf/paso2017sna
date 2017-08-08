a539060c58790b1bfd6886bcb40d9e38537f332e
Alejandro Zamora Fonseca <alejandrozf@alejandrozf.pc>
2017-08-08 01:49:48 -0300

    - Ordenando el código - Creando funciones para obtener y modificar credenciales de conexiones activas - Esbozando funcion para obtener tweets por usuario

--- twitter/src/get_tweets/src/get_tweets/core.clj
+++ twitter/src/get_tweets/src/get_tweets/core.clj
@@ -14,18 +14,49 @@
   (:import [twitter.callbacks.protocols AsyncSingleCallback])
   (:gen-class))
 
+;; Mutable values, about connections state
+(def active-credentials (agent {}))
+(def current-index (agent 0))
+(def active-pages (agent {}))
+
+;; MongoDB connection
+(def mongo-host "mongodb://localhost/paso2017_async")
+(def mongo-port 27017)
+
+;; Reading credentials
 (def api-credentials (parse-stream (io/reader "credentials.json")))
-(def test-credentials (first api-credentials))
+(def len-credentials (count api-credentials))
 
-(def my-creds (make-oauth-creds (get test-credentials "consumer_key")
-                                (get test-credentials "consumer_secret")
-                                (get test-credentials "access_token")
-                                (get test-credentials "access_token_secret")))
+(defn choose-oauth-credentials
+  "Gives the credentials according 'index'"
+  [index]
+  (let [credential (nth api-credentials index)]
+    (make-oauth-creds (get credential "consumer_key")
+                      (get credential "consumer_secret")
+                      (get credential "access_token")
+                      (get credential "access_token_secret"))))
 
-(def user-id "845696348")
-(def user-timeline (statuses-user-timeline :oauth-creds my-creds
-                                           :params {:user-id user-id
-                                                    :count 5}))
+(defn switch-credential
+  "Swith to another credential"
+  []
+  (send-off current-index (fn [x] (mod (inc x) len-credentials)))
+  (when (false? (get @active-credentials @current-index false))
+    (format "Switching to API Credentials #%d" @current-index)
+    (send-off active-credentials assoc
+              @current-index (choose-oauth-credentials @current-index))))
+
+(defn get-timeline
+  "Gets all tweets from user in range of dates"
+  ([uid day]
+   "pass1")
+  ([uid from to]
+   "pass2"))
+
+;; (def user-id "845696348")
+;; (def user-timeline (statuses-user-timeline :oauth-creds my-creds
+;;                                            :params {:user-id user-id
+;;                                                     :count 5}))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
